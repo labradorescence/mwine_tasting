6e22fc4a77a9d8671ac7314a770c33a676761b5b
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-12-05 16:34:47 -0500

    {date, name, rating, occasion, acidity_level, color, aroma, taste, region, note, price } = this.props

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,70 +1,49 @@
 import React from 'react'
-import { Item } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
 
 export default class ReviewCard extends React.Component {
 
-    renderReviews = () => {
-                console.log(this.props.reviews)
-                return this.props.reviews.map(eachReview => {
-                    console.log(eachReview.aroma)
-                return (
-                <div>
-                <br></br>
-                <h3> Review </h3>
-                review date: {eachReview.date}<br></br>
-                review occasion: {eachReview.occasion}<br></br>
-                review aroma: {eachReview.aroma}<br></br>
-                review color: {eachReview.color}<br></br>
-                review acidity level: {eachReview.acidity_level}<br></br>
-                review taste: {eachReview.taste}<br></br>
-                <br></br>
-                </div>
-                )
-            })
-    }
+
     render(){
-        console.log(this.props.date)
-        // const {image, name, aroma } = this.props ??? WHAT DOES THIS DO?
+        //console.log(this.props.date)
+         const {date, name, rating, occasion, acidity_level, color, aroma, taste, region, note, price } = this.props 
+         //console.log(this.props)
         return (
-                <Item.Group relaxed>
-                    <Item>
-                    <div className="image-cropper">
-                    <Item.Image size='large' src={this.props.image} alt= {this.props.name} />
+            <Card>
+
+                    <div className="content">
+
+                        <div className="header">{name}</div>
+                       
+                        <div className="meta">
+                        <span className="right floated time">{date}</span>
+                        <span className="category">{price}</span>
+                        </div>
+
+                        <div className="description">
+                        <li>{aroma}</li>
+                        <li>{occasion}</li>
+                        <li>{color}</li>
+                        <li>{taste}</li>
+                        <li>{acidity_level}</li>
+                        <li>{note}</li>
+
+                        </div>
                     </div>
-                    <Item.Content verticalAlign='middle' >
-                    <br></br><br></br>
 
-                        <Item.Header> {this.props.name}</Item.Header>
-                        <Item.Description>
-                        date: {this.props.date}<br></br>
-                        name: {this.props.name}<br></br>
-                        rating: {this.props.rating}/5<br></br>
-                        occasion: {this.props.occasion}<br></br>
-                        acidity_level: {this.props.acidity_level}<br></br>
-                        color: {this.props.color}<br></br>
-
-                        aroma: {this.props.aroma}<br></br>
-                        taste: {this.props.taste}<br></br>
+                    <div className="extra content">
                         
-                        region: {this.props.region}<br></br>
-                        note: {this.props.note}<br></br>
+                        <div className="right floated">
+                            {region}
+                        </div>
 
-                        price: ${this.props.price}<br></br>                  
-                        
-                        {this.props.reviews? this.renderReviews() : null}
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        </Item.Description>
-                        <Item.Extra>
-                        </Item.Extra>
-                    </Item.Content>
-                    </Item>
-                </Item.Group>
+                        <div className="left floated">
+                        <span role="img" aria-label="star">⭐</span>{rating}
+                        </div>
+                    
+                    </div>
+
+            </Card>
         )
     }
 }
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -5,11 +5,16 @@ export default class ReviewForm extends React.Component {
 
     state = {
         date: '',
-        occasion: "",
-        aroma: '',
-        color: '',
+        name: '',
+        rating: '',
+        occasion: '',
         acidity_level: '',
-        taste: ''
+        color: '',
+        aroma: '',
+        taste: '',
+        region: '',
+        note: '',
+        price: ''
     }
 
     handleInputChange = e => {
@@ -25,6 +30,11 @@ export default class ReviewForm extends React.Component {
 
         const bodyData = {
             "date": this.state.date,
+            "name": this.state.name,
+            "rating": this.state.rating,
+            "region": this.state.region,
+            "note": this.state.note,
+            "pricd": this.state.price,
             "occasion": this.state.occasion,
             "aroma": this.state.aroma,
             "color": this.state.color,
@@ -49,10 +59,7 @@ export default class ReviewForm extends React.Component {
     render(){
         return (
             <div className="form">
-                 
-                 
 
-                 
                  <img src ="https://res.cloudinary.com/devzsgipb/image/upload/v1596194143/ocy6vpyax2wiijbt7xcn.jpg" alt = "wine smell wheel"></img>
                 <h1>* S E E * S W I R L * S N I F F * S I P * S A V O R *</h1>
 
@@ -71,18 +78,26 @@ export default class ReviewForm extends React.Component {
                 />
 
                 <Form.Input 
-                fluid label="occasion"
-                placeholder="occasion"
-                name="occasion"
-                value={this.state.occasion}
+                fluid label="name"
+                placeholder="name"
+                name="name"
+                value={this.state.name}
                 onChange={this.handleInputChange}
                 />
 
                 <Form.Input 
-                fluid label="aroma"
-                placeholder="aroma"
-                name="aroma"
-                value={this.state.aroma}
+                fluid label="region"
+                placeholder="region"
+                name="region"
+                value={this.state.region}
+                onChange={this.handleInputChange}
+                />
+
+                <Form.Input 
+                fluid label="occasion"
+                placeholder="occasion"
+                name="occasion"
+                value={this.state.occasion}
                 onChange={this.handleInputChange}
                 />
 
@@ -95,10 +110,10 @@ export default class ReviewForm extends React.Component {
                 />
 
                 <Form.Input 
-                fluid label="acidity level"
-                placeholder="acidity level"
-                name="acidity_level"
-                value={this.state.acidity_level}
+                fluid label="aroma"
+                placeholder="aroma"
+                name="aroma"
+                value={this.state.aroma}
                 onChange={this.handleInputChange}
                 />
 
@@ -107,6 +122,38 @@ export default class ReviewForm extends React.Component {
                 placeholder="taste"
                 name="taste"
                 value={this.state.taste}
+                onChange={this.handleInputChange}
+                />
+
+                <Form.Input 
+                fluid label="acidity level"
+                placeholder="acidity level"
+                name="acidity_level"
+                value={this.state.acidity_level}
+                onChange={this.handleInputChange}
+                />
+
+                <Form.Input 
+                fluid label="note"
+                placeholder="note"
+                name="note"
+                value={this.state.note}
+                onChange={this.handleInputChange}
+                />
+
+                <Form.Input 
+                fluid label="price"
+                placeholder="price"
+                name="price"
+                value={this.state.price}
+                onChange={this.handleInputChange}
+                />
+
+                <Form.Input 
+                fluid label="rating"
+                placeholder="rating"
+                name="rating"
+                value={this.state.rating}
                 onChange={this.handleInputChange}
                 />
 
